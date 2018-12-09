c805af6eb076b0c3e5bbe11d68f8daabfb82682b
nuraalam <nuraalam939@gmail.com>
2018-12-09 19:15:54 +0600

    MongoDB Connection and schema added

--- app.js
+++ app.js
@@ -4,17 +4,35 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+//Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname, "public")));
 
+//View Engine
 app.set("view engine","ejs");
 
+// Mongodb database connection
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/hr-app", {useNewUrlParser : true});
+
+let db = mongoose.connection;
+
+db.once("open",function(){
+    console.log("Connected to MongoDB");
+});
+
+db.on("error",function(err){
+    console.log("error");
+});
+
+// Home Route
 app.get("/",function(req,res){
     res.render("login");
 });
 
 
+// Start the server
 app.listen(port,function(){
     console.log("Server started on port " + port);
 });
---
+++ models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+let UserSchema = mongoose.Schema({
+    name : 
+    {
+        type : String,
+        required : true
+    },
+    nid : 
+    {
+        type : Number,
+        required : true
+    },
+    birth_date :
+    {
+        type : String,
+        required : true
+    },
+    present_address : 
+    {
+        type : String,
+        required : true
+    },
+    permanent_address : 
+    {
+        type : String,
+        required : true
+    },
+    blood_group : 
+    {
+        type : String,
+        required : true
+    },
+    profile_photo : 
+    {
+        type : String,
+        required : true
+    },
+    passport_photo : 
+    {
+        type : String,
+        required : true
+    },
+    isAdmin : 
+    {
+        type : Boolean,
+        required : true,
+        default : 0
+    },
+
+    password : 
+    {
+        type : String,
+        required : true
+    }
+});
+
+let User = module.exports = mongoose.model("User",UserSchema);
