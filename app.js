46b449b0918cab1bbb2f791b37979e5288d61def
nuraalam <nuraalam939@gmail.com>
2018-12-10 02:43:08 +0600

    Login feature added

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const passport = require("passport");
 const app = express();
 const port = 3000;
 
-//Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-    secret : "keyboard cat",
-    resave : true,
-    saveUninitialized : true
-}));
-app.use(require('connect-flash')());
-//View Engine
-app.set("view engine","ejs");
-
 // Mongodb database connection
-const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/hr-app", {useNewUrlParser : true});
 
 let db = mongoose.connection;
@@ -32,18 +20,36 @@ db.on("error",function(err){
     console.log("error");
 });
 
-// Global Variables
+//Middleware
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+    secret : "keyboard cat",
+    resave : true,
+    saveUninitialized : true
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('connect-flash')());
 app.use(function(req,res,next){
     res.locals.errors = null;
     res.locals.msg = null;
     res.locals.messages = require('express-messages')(req, res);
+    res.locals.user = req.user || null;
     next();
 });
+//View Engine
+app.set("view engine","ejs");
+
+
+
+
 
 // Home Route
 app.get("/",function(req,res){
-    res.render("login");
+    res.render("home");
 });
 
 //Other Routes
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,12 +1,79 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const {check, validationResult} = require("express-validator/check");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+
+let UserModel = require("../models/userModel");
 
 router.get("/",function(req,res){
-    res.render("login");
+    if(req.isAuthenticated())
+    {
+        res.redirect("/");
+    }
+    else
+        res.render("login");
 });
 
-router.post("/",function(req,res){
-   
+passport.use(new LocalStrategy(
+	function (username, password, done) {
+        let query = {email : username};
+		UserModel.findOne(query, function (err, user) {
+			if (err) throw err;
+			if (!user) {
+				return done(null, false, { message: 'No User Found' });
+			}
+
+			bcrypt.compare(password, user.password, function (err, isMatch) {
+				if (err) throw err;
+				if (isMatch) {
+					return done(null, user);
+				} else {
+					return done(null, false, { message: 'Wrong password' });
+				}
+			});
+		});
+	}));
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
 });
+
+passport.deserializeUser(function (id, done) {
+	UserModel.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+
+router.post("/data",[
+    check("username").not().isEmpty().withMessage("Email is required"),
+    check("password").not().isEmpty().withMessage("Password is required")
+],function(req,res,next){
+   
+    let errors = validationResult(req);
+
+    if(!errors.isEmpty())
+    {
+        res.render("login",{
+            errors : errors.array()
+        });
+    }    
+    else
+    {
+        passport.authenticate("local",{
+            successRedirect : "/",
+            failureRedirect : "/login",
+            failureFlash : true
+        })(req,res,next);
+    }
+});
+
+router.get("/logout",function(req,res){
+    req.logout();
+    req.flash("success","You are now logged out");
+    res.redirect("/login");
+})
 
 module.exports = router;
