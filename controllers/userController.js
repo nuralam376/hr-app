bd859c82462f05ad3ea72e31153be2e35a0f2b14
Nur <nuraalam939@gmail.com>
2018-12-10 17:23:08 +0600

    Access Control Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,30 +4,63 @@ const router = express.Router();
 
 let UserModel = require("../models/userModel");
 
-router.get("/",function(req,res){
-    if(req.isAuthenticated())
+router.get("/",ensureAuthenticated,function(req,res){
+    UserModel.find({},function(err,users){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("users/index",{
+                users : users
+            });
+        }
+    });
+});
+
+
+
+router.get("/edit/:id",ensureAuthenticated,function(req,res){
+  if(req.user.isAdmin)
+  {
+    let query = {_id : req.params.id};
+
+    UserModel.findOne(query,function(err,user){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render("users/edit",{
+                newUser : user
+            });
+        }
+    });
+  }
+  else
+  {
+      req.flash("danger","Unauthorize Access");
+      res.redirect("/user");
+  }
+
+});
+
+router.get("/delete/:id",ensureAuthenticated,function(req,res){
+    if(req.user.isAdmin)
     {
-        UserModel.find({},function(err,users){
-            if(err)
-            {
-                console.log(err);
-            }
-            else
-            {
-                res.render("users/index",{
-                    users : users
-                });
-            }
-        });
+        res.redirect("/");
     }
     else
     {
-        req.flash("danger","Please login first");
-        res.redirect("/login");
-    }    
+        req.flash("danger","Unauthorize Access");
+        res.redirect("/user");
+    }
+  
 });
 
-router.get("/:id",function(req,res){
+router.get("/:id",ensureAuthenticated,function(req,res){
     let query = {_id : req.params.id};
 
     UserModel.findOne(query,function(err,user){
@@ -38,10 +71,23 @@ router.get("/:id",function(req,res){
         else
         {
             res.render("users/view",{
-                user : user
+                newUser : user
             });
         }
     });
-});
+});  
+
+function ensureAuthenticated(req,res,next)
+{
+    if(req.isAuthenticated())
+    {
+        next();
+    }
+    else
+    {
+        req.flash("danger","Please login first");
+        res.redirect("/login");
+    }
+}
 
 module.exports = router;
