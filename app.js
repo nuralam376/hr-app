b2b0f28e5927ef31c698034b77118e4dc19d77ad
Nur <nuraalam939@gmail.com>
2018-12-18 12:11:14 +0600

    Heroku Database setup

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8000;
 
 // Mongodb database connection
-mongoose.connect("mongodb://localhost/hr-app", {useNewUrlParser : true});
+mongoose.connect("mongodb://hr-app:hr123456@ds117509.mlab.com:17509/hr-app", {useNewUrlParser : true});
 
 let db = mongoose.connection;
 
