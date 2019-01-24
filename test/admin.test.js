635f4d274dc289caa7ccf57275c50d56b2ffda0d
Nur <nuraalam939@gmail.com>
2019-01-24 11:36:49 +0600

    Executed Admin's Model Unit Test

--- test/admin.test.js
+++ test/admin.test.js
@@ -1,11 +1,47 @@
 const mocha = require("mocha");
 const assert = require("assert");
+const mongoose = require("mongoose");
+
+/** Database Configuration File */
+const config = require("../config/database");
+
+/** Database Configuration Settings */
+mongoose.connect(config.database, {useNewUrlParser : true});
+
+const db = mongoose.connection;
+
+db.once("open",function(){
+    console.log("Connected to MongoDB");
+});
+
+db.on("error",function(err){
+    console.log("error");
+});
 
 const AdminModel = require("../models/AdminModel");
 
 describe("Cheks Admin Model Data", () => {
-    it("should return Admin Data", async() => {
-        let admin = await AdminModel.find({name : "Super Admin 1"});
-        assert.equal(admin.name,"Super Admin 1");
+
+    beforeEach(async() => {
+        let admin = new AdminModel({
+            name : "Abcd",
+            email : "abc@gmail.com",
+            contact : 1986322,
+            address : "Lorem Ipsum",
+            profile_photo : "dummy.jpeg",
+            password : "1233456"
+        });
+
+        await admin.save();
     });
-});
+
+    afterEach(async() => {
+        await AdminModel.deleteOne({email : "abc@gmail.com"});
+        db.close();
+    });
+
+    it("should return Admin's Data", async() =>  {
+        let admin = await AdminModel.findOne({name : "Abcd"});
+        assert.equal(admin.name,"Abcd");
+    });
+}); 
