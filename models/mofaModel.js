df7582d11030d18709d54fc848cecb5354ef47a3
Nur A Alam <nuraalam939@gmail.com>
2019-02-26 13:34:55 +0600

    Implemented Mofa Model

---
+++ models/mofaModel.js
@@ -0,0 +1,58 @@
+/** Mongoose Module */
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+/**  Mofa Collection */
+const mofaSchema = new Schema({
+    health_payment : 
+    {
+        type : Number,
+        required : true,
+        default : 1100 
+    },
+    embassy_payment : 
+    {
+        type : Number,
+        required : true,
+        default : 1200
+    },
+    type : 
+    {
+        type : Number,
+        required : true,
+        default : 100
+    },
+    e_number :
+    {
+        type : Number,
+        required : true
+    },
+    pax : 
+    {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    group : 
+    {
+        type : Schema.Types.ObjectId,
+        ref : "Group",
+        required : true
+    },
+    created_at : 
+    {
+        type : Date,
+        required : false,
+        default : Date.now()
+    },
+    updated_at : 
+    {
+        type : Date,
+        required : true,
+        default : Date.now()
+    }
+});
+
+const Mofa = module.exports = mongoose.model("Mofa",mofaSchema);
