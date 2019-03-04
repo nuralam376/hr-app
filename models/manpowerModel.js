20929b4b2efc824c2fbfe0949e62d7eab8eed2e7
Nur A Alam <nuraalam939@gmail.com>
2019-03-04 18:13:47 +0600

    Implemented Manpower Model

---
+++ models/manpowerModel.js
@@ -0,0 +1,55 @@
+/** Mongoose Module */
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+/** Manpower Schema */
+const manpowerSchema = new Schema({
+    ready : 
+    {
+        type : Number,
+        required : true,
+        default : 0
+    },
+    clearance_date : 
+    {
+        type : Date,
+        required : false,
+    },
+    card_no : 
+    {
+        type : String,
+        required : false
+    },
+    card_photo : 
+    {
+        type : String,
+        required : false
+    },
+    pax : 
+    {
+        type : Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    company : 
+    {
+        type : Schema.Types.ObjectId,
+        ref : "Company",
+        required : true
+    },
+    created_at : 
+    {
+        type : Date,
+        required : true,
+        default : Date.now()
+    },
+    updated_at : 
+    {
+        type : Date,
+        required : true,
+        default : Date.now()
+    }
+});
+
+const Manpower = module.exports = mongoose.model("Manpower",manpowerSchema);
