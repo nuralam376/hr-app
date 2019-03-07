7a2c6592454683e595f493e952a1bf82e1d6bf76
Nur A Alam <nuraalam939@gmail.com>
2019-03-07 18:36:38 +0600

    Added Admin Test using jest

---
+++ tests/admin.test.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+const config = require("../config/database");
+const db = mongoose.connection;
+const AdminModel = require("../models/AdminModel");
+
+beforeAll(() => {
+    /** Database Configuration File */
+    db.once("open",function(){
+        console.log("Connected to MongoDB");
+
+    });
+    db.on("error",function(err){
+        console.log("error");
+    });
+})
+
+
+afterAll(() => {
+    db.close();
+})
+
+
+
+
+
+describe("Cheks Admin Model Data", () => {
+    test("should add admin data", async() => {
+        let admin = new AdminModel({
+            name : "Abcd",
+            email : "abc@gmail.com",
+            contact : 1986322,
+            address : "Lorem Ipsum",
+            profile_photo : "dummy.jpeg",
+            password : "1233456",
+            created_at : Date.now(),
+            updated_at : Date.now(),
+        });
+
+        let newAdmin = await admin.save();
+        let adminData = await AdminModel.findById(newAdmin._id);
+        expect.assertions(1);
+        expect(adminData._id).toEqual(newAdmin._id);
+    });
+
+    test("should update admin data", async() => {
+        expect.assertions(1);
+        let adminData = await AdminModel.findOne({name : "Abcd"});
+        let admin = {};
+        admin.contact = 789765456;
+        await AdminModel.updateOne({_id : adminData._id},admin);
+        adminData = await AdminModel.findOne({name : "Abcd"});
+        expect(adminData.contact).toEqual(789765456);
+    });
+
+    test("should delete the admin data", async() => {
+        expect.assertions(1);
+        let admin = await AdminModel.deleteOne({name : "Abcd"});
+        let adminData = await AdminModel.findOne({name : "Abcd"});
+        expect(adminData).toBeNull();
+    });
+}); 
