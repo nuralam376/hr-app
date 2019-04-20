f0631fcd3877ebd0b2d9df868543459302ea6baf
Nur A Alam <nuraalam939@gmail.com>
2019-04-20 12:01:55 +0600

    Added Supplier Line Chart

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,4 +1,3 @@
-
 /** Supplier Model Schema */
 const SupplierModel = require("../models/supplierModel");
 
@@ -8,48 +7,67 @@ const UserModel = require("../models/userModel");
 /** Gets S3 File */
 const s3GetFile = require("../util/getS3File");
 
-exports.getDashboard = async(req,res) => {
-    try{
-        let totalSupplier = await SupplierModel.countDocuments();
-        let totalUser = await UserModel.countDocuments();
+exports.getDashboard = async (req, res) => {
+  try {
+    let totalSupplier = await SupplierModel.countDocuments();
+    let totalUser = await UserModel.countDocuments();
 
-        
+    res.render("dashboard", {
+      totaluser: totalUser,
+      totalSupplier: totalSupplier
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    
-        res.render("dashboard",{
-            totaluser : totalUser,
-            totalSupplier : totalSupplier
-        });
-    }
-    catch(err){
-        console.log(err);
-    }
-    
-}
+exports.getChartData = async (req, res) => {
+  try {
+    let totalPAX = await getChartResult(UserModel);
+    let totalSupplier = await getChartResult(SupplierModel);
 
+    let totals = {
+      totalPAX,
+      totalSupplier
+    };
 
-exports.getChartData = async(req,res) => {
-    try
-    {
-        let totalPAX = await UserModel.aggregate([
-            {
-                $group : {
-                    _id : {
-                        create : {
-                            $month : "$created_at"
-                        },
-                    },
-                    total : {
-                        $sum : 1
-                    }
-                }
-            }
-        ]);
+    return res.jsonp(totals);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-        return res.jsonp(totalPAX);
-    }
-    catch(err)
-    {
-        console.log(err);
-    }
-}
+const getChartResult = async Model => {
+  try {
+    let total = await Model.aggregate([
+      {
+        $project: {
+          year: {
+            $year: "$created_at"
+          },
+          month: {
+            $month: "$created_at"
+          }
+        }
+      },
+      {
+        $match: {
+          year: new Date().getFullYear()
+        }
+      },
+      {
+        $group: {
+          _id: {
+            create: "$month"
+          },
+          total: {
+            $sum: 1
+          }
+        }
+      }
+    ]);
+    return total;
+  } catch (err) {
+    console.log(err);
+  }
+};
--- public/js/main.js
+++ public/js/main.js
@@ -1,341 +1,331 @@
-$(document).ready(function(){
-    $(".delete-user").on("click",function(e){
-       ajaxDelete(e,"pax");
-    });
+$(document).ready(function() {
+  $(".delete-user").on("click", function(e) {
+    ajaxDelete(e, "pax");
+  });
 
-    $(".delete-supplier").on("click",function(e){
-        ajaxDelete(e,"supplier");
-    });
+  $(".delete-supplier").on("click", function(e) {
+    ajaxDelete(e, "supplier");
+  });
 
-    $(".delete-admin").on("click",function(e){
-        ajaxDelete(e,"admin");
-      
-    });
+  $(".delete-admin").on("click", function(e) {
+    ajaxDelete(e, "admin");
+  });
 
-    $(".delete-zone").on("click",function(e){
-        ajaxDelete(e,"zone");
-      
-    });
+  $(".delete-zone").on("click", function(e) {
+    ajaxDelete(e, "zone");
+  });
 
-    $(".delete-group").on("click",function(e){
-        ajaxDelete(e,"group");
-      
-    });
-    
-    
-    $(".delete-medical").on("click",function(e){
-        ajaxDelete(e,"medical");      
-    });
-    $(".delete-mofa").on("click",function(e){
-        ajaxDelete(e,"mofa");      
-    });
+  $(".delete-group").on("click", function(e) {
+    ajaxDelete(e, "group");
+  });
 
-    $(".delete-stamping").on("click",function(e){
-        ajaxDelete(e,"stamping");      
-    });
+  $(".delete-medical").on("click", function(e) {
+    ajaxDelete(e, "medical");
+  });
+  $(".delete-mofa").on("click", function(e) {
+    ajaxDelete(e, "mofa");
+  });
 
-    $(".delete-tc").on("click",function(e){
-        ajaxDelete(e,"tc");      
-    });
+  $(".delete-stamping").on("click", function(e) {
+    ajaxDelete(e, "stamping");
+  });
 
-    $(".delete-manpower").on("click",function(e){
-        ajaxDelete(e,"manpower");      
-    });
-    
-    $(".delete-flight").on("click",function(e){
-        ajaxDelete(e,"flight");      
-    });
+  $(".delete-tc").on("click", function(e) {
+    ajaxDelete(e, "tc");
+  });
 
-    $(".delete-delivery").on("click",function(e){
-        ajaxDelete(e,"delivery");      
-    });
+  $(".delete-manpower").on("click", function(e) {
+    ajaxDelete(e, "manpower");
+  });
 
-    function ajaxDelete(e,route)
-    {
-        e.preventDefault();
-        $.confirm({
-            title: 'Delete Confirmation!',
-            content: 'Are you sure you want to delete it permanently?',
-            type: 'red',
-            buttons: {
-                confirm: function () {
-                    $target = $(e.target);
-                    const id = $target.attr("data-id");
-                    const csrf = $target.attr("data-csrf");
-                
-                    $.ajax({
-                        type : "Delete",
-                        url : "/"+route+"/delete/" + id,
-                        data : 
-                        {
-                            _csrf : csrf
-                        },
-                        success : function(response)
-                        {
-                            console.log(response);
-                            if(route == "medical")
-                            {
-                                window.location.href = "/"+route+"/all";
-                            }
-                            else
-                            {
-                                window.location.href = "/"+route+"";
-                            }
-                        },
-            
-                        error : function(err)
-                        {
-                            if(route == "medical")
-                            {
-                                window.location.href = "/"+route+"/all";
-                            }
-                            else
-                            {
-                                window.location.href = "/"+route+"";
-                            }
-                        }
-                    });
-                },
-                cancel: function () {
-                    $.alert('Canceled!');
-                }
-               
-            }
-        });
+  $(".delete-flight").on("click", function(e) {
+    ajaxDelete(e, "flight");
+  });
 
-    }
+  $(".delete-delivery").on("click", function(e) {
+    ajaxDelete(e, "delivery");
+  });
 
-    function readURL(input,id) {
+  function ajaxDelete(e, route) {
+    e.preventDefault();
+    $.confirm({
+      title: "Delete Confirmation!",
+      content: "Are you sure you want to delete it permanently?",
+      type: "red",
+      buttons: {
+        confirm: function() {
+          $target = $(e.target);
+          const id = $target.attr("data-id");
+          const csrf = $target.attr("data-csrf");
 
-        if (input.files && input.files[0]) {
-          var reader = new FileReader();
-      
-          reader.onload = function(e) {
-            $(id).attr('src', e.target.result);
-          }
-      
-          reader.readAsDataURL(input.files[0]);
-        }
-      }
-      
-      $("#profile_photo").change(function() {
-        readURL(this,"#photo");
-      });
-
-      $("#passport_photo").change(function() {
-        readURL(this,"#photo1");
-      });
-      $("#enjazit").change(function() {
-        readURL(this,"#photo");
-      });
-      $("#experience_image").change(function() {
-        readURL(this,"#photo2");
-      });
-      $("#slip").change(function() {
-        readURL(this,"#photo");
-      });
-      $("#pc_image").change(function() {
-        readURL(this,"#photo");
-      });
-      $("#card_photo").change(function() {
-        readURL(this,"#photo");
-      });
-      
-      function getZones()
-      {
-        $.ajax({
-            type : "GET",
-            url : "/zone/names",
-            success : function(response)
-            {
-                let availableZones = [];
-                let zoneInfo;
-                response.forEach(zone => {
-                    zoneInfo = zone.name;
-                    availableZones.push(zoneInfo);
-                });
-                showResult(availableZones);
+          $.ajax({
+            type: "Delete",
+            url: "/" + route + "/delete/" + id,
+            data: {
+              _csrf: csrf
+            },
+            success: function(response) {
+              console.log(response);
+              if (route == "medical") {
+                window.location.href = "/" + route + "/all";
+              } else {
+                window.location.href = "/" + route + "";
+              }
             },
 
-            error : function(err)
-            {
-                availableTags = err;
+            error: function(err) {
+              if (route == "medical") {
+                window.location.href = "/" + route + "/all";
+              } else {
+                window.location.href = "/" + route + "";
+              }
             }
-        });
-      }
-
-    $("#zone").on("focus",function(){
-        getZones();
-    });
-
-    function getOccupations()
-      {
-        $.ajax({
-            type : "GET",
-            url : "/mofa/groups",
-            success : function(response)
-            {
-                let availableGroups = [];
-                let groupInfo;
-                response.forEach(group => {
-                    groupInfo = group.occupation;
-                    availableGroups.push(groupInfo);
-                });
-                showGroup(availableGroups);
-            },
-
-            error : function(err)
-            {
-                availableTags = err;
-            }
-        });
-      }
-
-    $("#occupation").on("focus",function(){
-        getOccupations();
-    })
-
-
-    function showResult(availableZones)
-    {
-        $("#zone").autocomplete({
-            source: availableZones,
-            selectFirst: true, 
-            minLength: 0,
-            focus : function(event,ui) {
-                $(this).autocomplete("search", $(this).val());
-            },
-            select : function(event,ui){
-                $(this).val(ui.item.label);
-            }
-        })
-    }
-
-    function showGroup(availableGroups)
-    {
-        $("#occupation").autocomplete({
-            source: availableGroups,
-            selectFirst: true, 
-            minLength: 0,
-            focus : function(event,ui) {
-                $(this).autocomplete("search", $(this).val());
-            },
-            select : function(event,ui){
-                $(this).val(ui.item.label);
-            }
-        })
-    }
-
-    function checkUncheck() { 
-      
-        if ( $('#experience:checked').length > 0) {
-            $("#experience-div").show();
-        } else {
-            $("#experience-div").hide();
-        }
-    
-    }
-
-    $("#experience-div").hide();
-    $("#experience").on( "click", checkUncheck );
-
-    $("#unfit").hide();
-    $("#interview").hide();
-
-    function showDiv()
-    {
-        if($("#status").val() == "unfit")
-        {
-            $("#unfit").show();
-            $("#interview").hide();
-        }
-        else if($("#status").val() == "interview")
-        {
-            $("#interview").show();
-            $("#unfit").hide();
-        }
-        else
-        {
-            $("#unfit").hide();
-            $("#interview").hide();
-        }
-    }
-
-    $("#status").on("click",showDiv);
-    
-
-    $.ajax({
-        type : "GET",
-        url : "/dashboard/data",
-        success : function(response)
-        {
-            let dataArray = [];
-            
-            response.map(pax => {
-                dataArray[pax._id.create - 1] = pax.total;
-            });
-
-            if(typeof dataArray[0] == "undefined")
-                dataArray[0] = 0;
-            
-
-                var config = {
-                    type: 'line',
-                    data: {
-                        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','November','December'],
-                        datasets: [{
-                            label: 'PAX',
-                            fill: false,
-                            borderColor: "#3e95cd",
-                            data: dataArray
-                        }]
-                    },
-                    options: {
-                        responsive: true,
-                        title: {
-                            display: true,
-                            text: 'Total Registered'
-                        },
-                        tooltips: {
-                            mode: 'index',
-                            intersect: false,
-                        },
-                        hover: {
-                            mode: 'nearest',
-                            intersect: true
-                        },
-                        scales: {
-                            xAxes: [{
-                                display: true,
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Month'
-                                },
-                            }],
-                            yAxes: [{
-                                display: true,
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: 'Value',
-                                },
-                                ticks: {
-                                    reverse: false,
-                                    stepSize: 1
-                                  },
-                            }]
-                        },
-                    }
-                };
-        
-               
-                var ctx = document.getElementById('lineChart').getContext('2d');
-                new Chart(ctx, config);
-                
+          });
         },
-        error : function(err)
-        {
-            console.log(err);
+        cancel: function() {
+          $.alert("Canceled!");
         }
+      }
+    });
+  }
+
+  function readURL(input, id) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        $(id).attr("src", e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#profile_photo").change(function() {
+    readURL(this, "#photo");
+  });
+
+  $("#passport_photo").change(function() {
+    readURL(this, "#photo1");
+  });
+  $("#enjazit").change(function() {
+    readURL(this, "#photo");
+  });
+  $("#experience_image").change(function() {
+    readURL(this, "#photo2");
+  });
+  $("#slip").change(function() {
+    readURL(this, "#photo");
+  });
+  $("#pc_image").change(function() {
+    readURL(this, "#photo");
+  });
+  $("#card_photo").change(function() {
+    readURL(this, "#photo");
+  });
+
+  function getZones() {
+    $.ajax({
+      type: "GET",
+      url: "/zone/names",
+      success: function(response) {
+        let availableZones = [];
+        let zoneInfo;
+        response.forEach(zone => {
+          zoneInfo = zone.name;
+          availableZones.push(zoneInfo);
+        });
+        showResult(availableZones);
+      },
+
+      error: function(err) {
+        availableTags = err;
+      }
+    });
+  }
+
+  $("#zone").on("focus", function() {
+    getZones();
+  });
+
+  function getOccupations() {
+    $.ajax({
+      type: "GET",
+      url: "/mofa/groups",
+      success: function(response) {
+        let availableGroups = [];
+        let groupInfo;
+        response.forEach(group => {
+          groupInfo = group.occupation;
+          availableGroups.push(groupInfo);
+        });
+        showGroup(availableGroups);
+      },
+
+      error: function(err) {
+        availableTags = err;
+      }
+    });
+  }
+
+  $("#occupation").on("focus", function() {
+    getOccupations();
+  });
+
+  function showResult(availableZones) {
+    $("#zone").autocomplete({
+      source: availableZones,
+      selectFirst: true,
+      minLength: 0,
+      focus: function(event, ui) {
+        $(this).autocomplete("search", $(this).val());
+      },
+      select: function(event, ui) {
+        $(this).val(ui.item.label);
+      }
+    });
+  }
+
+  function showGroup(availableGroups) {
+    $("#occupation").autocomplete({
+      source: availableGroups,
+      selectFirst: true,
+      minLength: 0,
+      focus: function(event, ui) {
+        $(this).autocomplete("search", $(this).val());
+      },
+      select: function(event, ui) {
+        $(this).val(ui.item.label);
+      }
+    });
+  }
+
+  function checkUncheck() {
+    if ($("#experience:checked").length > 0) {
+      $("#experience-div").show();
+    } else {
+      $("#experience-div").hide();
+    }
+  }
+
+  $("#experience-div").hide();
+  $("#experience").on("click", checkUncheck);
+
+  $("#unfit").hide();
+  $("#interview").hide();
+
+  function showDiv() {
+    if ($("#status").val() == "unfit") {
+      $("#unfit").show();
+      $("#interview").hide();
+    } else if ($("#status").val() == "interview") {
+      $("#interview").show();
+      $("#unfit").hide();
+    } else {
+      $("#unfit").hide();
+      $("#interview").hide();
+    }
+  }
+
+  $("#status").on("click", showDiv);
+
+  $.ajax({
+    type: "GET",
+    url: "/dashboard/data",
+    success: function(response) {
+      let paxData = getChartData(response.totalPAX);
+      let supplierData = getChartData(response.totalSupplier);
+      let config = {
+        type: "line",
+        data: {
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+          ],
+          datasets: [
+            {
+              label: "PAX",
+              fill: false,
+              borderColor: "#3e95cd",
+              data: paxData
+            },
+            {
+              label: "Supplier",
+              fill: false,
+              borderColor: "green",
+              data: supplierData
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          title: {
+            display: true,
+            text: "Total Registered - " + new Date().getFullYear()
+          },
+          tooltips: {
+            mode: "index",
+            intersect: false
+          },
+          hover: {
+            mode: "nearest",
+            intersect: true
+          },
+          scales: {
+            xAxes: [
+              {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: "Month"
+                }
+              }
+            ],
+            yAxes: [
+              {
+                display: true,
+                scaleLabel: {
+                  display: true,
+                  labelString: "Value"
+                },
+                ticks: {
+                  reverse: false,
+                  stepSize: 1
+                }
+              }
+            ]
+          }
+        }
+      };
+
+      var ctx = document.getElementById("lineChart").getContext("2d");
+      new Chart(ctx, config);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+
+  function getChartData(responseData) {
+    let chartData = [];
+
+    responseData.map(data => {
+      chartData[data._id.create - 1] = data.total;
     });
 
+    for (let i = 0; i < 12; i++)
+      if (typeof chartData[i] == "undefined") chartData[i] = 0;
+
+    return chartData;
+  }
 });
