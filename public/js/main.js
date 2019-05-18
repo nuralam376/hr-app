bade12adb7bc8d266cb240d9fe970c7ba4eb7dd6
Nur A Alam <nuraalam939@gmail.com>
2019-05-18 13:04:40 +0600

    Changed PAX client moment time

--- public/js/main.js
+++ public/js/main.js
@@ -461,8 +461,19 @@ $(document).ready(function () {
     });
     const val = $("#profile").data("val");
     const pax = moment(new Date(val)).format("llll");
-    console.log(pax);
+
+
     $("#profile").text(pax);
+    const count = $("#userCount").data("length");
+    console.log(count);
+    if (count) {
+      for (let i = 0; i < count; i++) {
+        let userId = $("#user-" + i).data("user");
+        console.log(userId);
+        let user = moment(new Date(userId)).format("llll");
+        $("#user-" + i).text(user);
+      }
+    }
 
     $('.sidebar-menu').tree();
     $('#myTable').DataTable({
