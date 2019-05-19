38769ba0d6b7a2c0a4ce4345698004416321694e
Nur A Alam <nuraalam939@gmail.com>
2019-05-19 11:47:55 +0600

    Fixed Admin Moment Time

--- public/js/main.js
+++ public/js/main.js
@@ -459,22 +459,6 @@ $(document).ready(function () {
         }
       });
     });
-    const val = $("#profile").data("val");
-    const pax = moment(new Date(val)).format("llll");
-
-
-    $("#profile").text(pax);
-    const count = $("#userCount").data("length");
-    console.log(count);
-    if (count) {
-      for (let i = 0; i < count; i++) {
-        let userId = $("#user-" + i).data("user");
-        console.log(userId);
-        let user = moment(new Date(userId)).format("llll");
-        $("#user-" + i).text(user);
-      }
-    }
-
     $('.sidebar-menu').tree();
     $('#myTable').DataTable({
       "ordering": false
