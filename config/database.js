7c696899f6a627aacbf7f03e511bd47147a2f9d3
Nur A Alam <nuraalam939@gmail.com>
2021-04-24 12:46:04 +0600

    updated database and s3

--- config/database.js
+++ config/database.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
 
 /** Database Configuration Settings */
-exports.database = mongoose.connect("mongodb://hr-app:hr123456@ds117509.mlab.com:17509/hr-app", {useNewUrlParser : true});
+exports.database = mongoose.connect(
+  "mongodb+srv://hr-user:TtigXTj0BL7XR7Ja@hr-app.nkghb.mongodb.net/hr-app?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
--- config/s3.js
+++ config/s3.js
@@ -1,4 +1,4 @@
 module.exports = {
-    accessKeyId : " AKIAJN432OQYDJ3ROBTA",
-    secretAccessKey : "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"
+  accessKeyId: " AKIAYBBL3Q465JO4XDWW",
+  secretAccessKey: "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",
 };
