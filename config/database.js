818f8c72df38eaeff1a8ffe808a6637decf45248
Nur A Alam <nuraalam376@gmail.com>
2022-11-22 17:27:13 +0600

    Changed database

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 /** Database Configuration Settings */
 exports.database = mongoose.connect(
-  "mongodb+srv://hr-user:TtigXTj0BL7XR7Ja@hr-app.nkghb.mongodb.net/hr-app?retryWrites=true&w=majority",
+  "mongodb+srv://hr_app_user:V35wJrdFrq0iyEqf@hr-app.nkghb.mongodb.net/hr-app?retryWrites=true&w=majority",
   { useNewUrlParser: true }
 );
